affe86774b8d5cc39b85163201f3c768b5b66e3c
mubeen <mubeenarman19@gmail.com>
2020-10-18 21:47:44 +0900

    variables and module

---
+++ variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+//DECLARE the variable a
+//assign value 100
+// declare & assign = initilization
+var a = 100
+
+//DECLARE there is a variable with the inentifier 'y'
+// and that variable with the identifier 'y' if of type int
+// assign the 0 value of type int to y
+// false for booleans, 0 for integers, 0.0 for float, "" for string
+// and nil for pointer, functions, interfaces, slices, channels and map
+var y int
+
+func main() {
+	fmt.Println("Hello world")
+
+	x := 50
+	fmt.Println(x)
+	fmt.Println(a)
+	fmt.Println(y)
+}
